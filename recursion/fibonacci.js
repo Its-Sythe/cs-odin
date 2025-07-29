88cd89d48496e6b81a64208cdc5382d36e295557
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-07-29 12:51:05 +0300

    Getting the array sucked, but fibs done

---
+++ recursion/fibonacci.js
@@ -0,0 +1,26 @@
+function fibs(n) {
+    if (n == 1) return 0;
+
+
+    let seq = [0, 1];
+    for (let i = 0; i < n; i++) {
+        let next = (seq.at(-1) + seq.at(-2))
+        if (seq.length != n) {
+            seq.push(next);
+        } else {
+            break;
+        }
+    }
+    return seq;
+}
+
+
+function fibsRec(n, seq = [0, 1]) {
+    if (seq.length == n) return seq;
+
+    let next = seq.at(-1) + seq.at(-2)
+    seq.push(next);
+    return fibsRec(n, seq)
+}
+
+console.log(fibsRec(8))
