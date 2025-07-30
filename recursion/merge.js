2a4577ab6823612b250f4c31b76edaeca8639421
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-07-30 07:58:45 +0300

    Finished merge sort

--- recursion/merge.js
+++ recursion/merge.js
@@ -1,6 +1,37 @@
+function merge(l, r) {
+    let result = []
+    
+    let  i = 0; j = 0;
+    // Check that both of the arrays are not empty
+    while ( i < l.length && j < r.length) {
+        // Check whether the element at i in the left array is less than 
+        // that of the right array
+        if (l[i] < r[j]) {
+            //if true push the left one to the results array
+            result.push(l[i])
+            //Increment the counter for the base case of the loop by 1
+            i++
+        // Otherwise do the opposite and also increment j by 1
+        } else {
+            result.push(r[j])
+            j++
+        }
+    }
+    
+    // Check for individual items in the arrays
+    for(i; i < l.length; i++) {
+        result.push(l[i])
+    }
+
+    for(j; j < r.length; j++) {
+        result.push(r[j])
+    }
+
+    return result;
+}
+
 function mergeSort(arr) {
     if (arr.length <= 1) {
-        console.log("Reached base case: ", arr)
         return arr;
     }
 
@@ -8,14 +39,7 @@ function mergeSort(arr) {
     let left = arr.slice(0, mid);
     let right = arr.slice(mid);
 
-    console.log(`Split into ${left} and ${right}`)
-
-    let sortLeft = mergeSort(left);
-    console.log("Ended left with: ", sortLeft)
-    let sortRight = mergeSort(right);
-    console.log("Ended right with: ", sortRight)
-    let sorted;
-    return sorted;
+    return merge(mergeSort(left), mergeSort(right))
 }
 
-mergeSort([1, 9, 5, 3, 7, 2])
+console.log(mergeSort([1, 9, 5, 3, 7, 2]))
