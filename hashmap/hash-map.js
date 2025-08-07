1d34cc5ba65d9f04e3fce91f8180a97a56c968a4
Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-08-07 11:13:29 +0000

    Added check for using set function

--- hashmap/hash-map.js
+++ hashmap/hash-map.js
@@ -1,10 +1,10 @@
 import { List } from './linked-list.js'
 class HashMap {
     constructor(capacity = 16, loadFactor = 0.8) {
-       this.capacity = capacity;
-       this.loadFactor = loadFactor;
-       this.buckets = new Array(capacity).fill(null) 
-       this.size = 0;
+        this.capacity = capacity;
+        this.loadFactor = loadFactor;
+        this.buckets = new Array(capacity).fill(null)
+        this.size = 0;
     }
 
     hash(key) {
@@ -21,14 +21,16 @@ class HashMap {
 
     set(key, value) {
         let list = new List()
-       let index = this.hash(key)
-       if (this.buckets[index] == null) {
-        list.append(key, value)
-        this.buckets[index] = list
-        return;
-       }
-       this.buckets[index].append(key, value)
-       console.log(this.buckets[index])
+        let index = this.hash(key)
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds")
+        }
+        if (this.buckets[index] == null) {
+            list.append(key, value)
+            this.buckets[index] = list
+            return;
+        }
+        this.buckets[index].append(key, value)
     }
 }
 
@@ -36,3 +38,4 @@ let map = new HashMap();
 map.set("Hello", "IDK")
 map.set("Sara", "Example")
 map.set("raSa", "Another Example")
+map.set("Sara", "New Example")
