ed4b66b9ab5116b4071969e76576734aaba73cab
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-08-11 23:13:35 +0300

    Put everything in classes and testing some delete ideas

---
+++ bst/main.js
@@ -0,0 +1,19 @@
+import { Tree } from './tree.js'
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+let tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree.buildTree(tree.arr)
+tree.insert(2)
+tree.delete(1)
+prettyPrint(tree.root)
--- bst/tree.js
+++ bst/tree.js
@@ -6,33 +6,101 @@ class Node {
     }
 }
 
-class Tree {
+export class Tree {
     constructor(arr) {
-        this.arr = arr
-        this.root = this.buildTree() || null;
+        this.arr = this.sortArr(arr);
+        this.root = this.buildTree(this.arr);
     }
 
     buildTree(arr) {
         if (arr.length == 0) return null;
         if (arr.length <= 1) return new Node(arr[0])
-        arr = this.sortArray(arr)
-
         let mid = Math.floor(arr.length / 2)
-        let left = arr.slice(0, mid + 1)
-        let right = arr.slice(mid + 1)
+        let left = arr.slice(0, mid)
+        let right = arr.slice(mid + 1);
 
         let root = new Node(arr[mid])
-        root.lChild = buildTree(left)
-        root.rChild = buildTree(right)
+        root.left = this.buildTree(left)
+        root.right = this.buildTree(right)
 
         return root;
     }
 
-    sortArray(arr) {
+    sortArr(arr) {
         return Array.from(new Set(arr.toSorted((a, b) => a - b)))
     }
-}
 
-let tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-tree.buildTree(tree.arr)
+    insert(value) {
+        let currNode = this.root;
+        if (currNode == null) {
+            return this.root = new Node(value)
+        }
+
+        let prevNode;
+
+        while(currNode != null) {
+            prevNode = currNode;
+
+            if (currNode.value == value) break;
+
+            if (currNode.value > value) {
+                currNode = currNode.left;
+            } else if (currNode.value < value) {
+                currNode = currNode.right;
+            }
+
+        }
+        if (prevNode.value > value) {
+            prevNode.left = new Node(value)
+        } else if (prevNode.value < value) {
+            prevNode.right = new Node(value)
+        }
+    }
+
+    delete(value) {
+        let currNode = this.root;
+        if (currNode == null) return;
+
+        let prevNode;
+
+        while (currNode != null) {
+            if (currNode.value == value) break;
+            prevNode = currNode
+            if (currNode.value > value) {
+                currNode = currNode.left;
+            } else if (currNode.value < value) {
+                currNode = currNode.right;
+            }
+        }
+        
+        //Delete a leaf.
+        if (currNode.left == null && currNode.right == null) {
+            if (prevNode.value > value) {
+                prevNode.left = null;
+            } else if (prevNode.value < value) {
+                prevNode.right = null;
+            }
+        }
+
+        //Delete a parent node with one child.
+        if (currNode.left != null) {
+            if (prevNode.value > currNode.left.value) {
+                prevNode.left = this.insert(currNode.left.value)
+            } else if (prevNode.value < currNode.left.value) {
+                prevNode.right = this.insert(currNode.left.value)
+            }
+        } else if (currNode.right != null) {
+            console.log("Reached here")
+            if (prevNode.value > currNode.value) {
+                console.log("Should be here")
+                prevNode.left = currNode.right
+                currNode = null
+                console.log(prevNode)
+            } else if (prevNode.value < currNod.right.value) {
+                prevNode.right = currNode.right
+            }
+        }
+      
+    }
+}
 
