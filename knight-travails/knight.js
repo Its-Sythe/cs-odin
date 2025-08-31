d1fd0407dd5f3c3166052a2d767c843f0e938eee
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-08-31 21:00:44 +0300

    Actually starting kt, thinking of ways to do this one, started with defining possible knight moves from pos(x,y)

---
+++ knight-travails/knight.js
@@ -0,0 +1,10 @@
+const possiblePos = [ // This possible movements a knight can make at given point (x,y)
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, -2],
+    [-1, 2]
+]
