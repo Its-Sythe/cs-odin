647b13d49002c5c4001005ca0a26ba86f09bc64f
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-09-04 15:22:22 +0300

    Make move function

--- knight-travails/knight.js
+++ knight-travails/knight.js
@@ -1,4 +1,4 @@
-const possiblePos = [ // This possible movements a knight can make at given point (x,y)
+const possibleMoves = [ // These are possible movements a knight can make at given point (x,y)
     [2, 1],
     [2, -1],
     [-2, 1],
@@ -8,3 +8,15 @@ const possiblePos = [ // This possible movements a knight can make at given poin
     [-1, -2],
     [-1, 2]
 ]
+
+function makeMove(pos) {
+    let allMoves = []
+    possibleMoves.forEach((move) => {
+        let moveMade = [(pos.at(0) + move.at(0)), (pos.at(1) + move.at(1))]
+        if ((moveMade.at(0) <= 7 && moveMade.at(0) >= 0) && (moveMade.at(1) <= 7 && moveMade.at(1) >= 0)) {
+            allMoves.push(moveMade)
+        }
+    }) 
+    return allMoves
+}
+
