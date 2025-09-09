8c37a4f58a10ae5ce7421673b48e81669ff8c95c
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-09-09 09:15:43 +0300

    Completed Assignment

--- knight-travails/knight.js
+++ knight-travails/knight.js
@@ -1,22 +1,48 @@
-const possibleMoves = [ // These are possible movements a knight can make at given point (x,y)
-    [2, 1],
-    [2, -1],
-    [-2, 1],
-    [-2, -1],
-    [1, 2],
-    [1, -2],
-    [-1, -2],
-    [-1, 2]
-]
+function knightMoves(start, end) {
+    const gallops = [
+        [1, 2], [1, -2], [-1, 2], [-1, -2],
+        [2, 1], [2, -1], [-2, 1], [-2, -1]
+    ];
 
-function makeMove(pos) {
-    let allMoves = []
-    possibleMoves.forEach((move) => {
-        let moveMade = [(pos.at(0) + move.at(0)), (pos.at(1) + move.at(1))]
-        if ((moveMade.at(0) <= 7 && moveMade.at(0) >= 0) && (moveMade.at(1) <= 7 && moveMade.at(1) >= 0)) {
-            allMoves.push(moveMade)
+    function validPos(pos) {
+        return pos[0] >= 0 && pos[0] <= 7 && pos[1] >= 0 && pos[1] <=7;
+    }
+
+    function makeGallop(pos) {
+        if (!validPos(pos)) return;
+
+        let allGallops = [];
+        for (let i = 0; i < gallops.length; i++) {
+            let gallop = [(pos[0] + gallops[i][0]), (pos[1] + gallops[i][1])];
+            if (validPos(gallop)) allGallops.push(gallop);
         }
-    }) 
-    return allMoves
+        return allGallops;
+    }
+
+    let queue = [[start, [start]]]
+    let visited = [start]
+
+    while (queue.length != 0) {
+        let [curr, path] = queue.shift();
+        let moves = makeGallop(curr);
+        for (let i = 0; i < moves.length; i++) {
+            let next = moves[i];
+            if (next[0] == end[0] && next[1] == end[1]) {
+                prettyPrint([...path, next])
+                return [...path, next]
+            }
+            if (!visited.some((node) => node[0] == next[0] && node[1] == next[1])) {
+                visited.push(next);
+                queue.push([next, [...path, next]])
+            }
+        }
+    }
 }
 
+function prettyPrint(path) {
+    console.log(`You made it in ${path.length - 1} moves! Here's your path: `);
+    for (let i = 0; i < path.length; i++) {
+        console.log(path[i])
+    }
+}
+knightMoves([0, 0], [7, 7])
